59442f2bc063b32cdda12113adf49c6f5075528a
SukkaW <isukkaw@gmail.com>
2021-06-11 01:24:58 +0800

    Print platform info

--- run.js
+++ run.js
@@ -1,32 +1,37 @@
-// Copyright 2018 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     <https://www.apache.org/licenses/LICENSE-2.0>
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-"use strict";
-
 const cp = require('child_process');
-const fs = require('fs');
+const listDir = require('@sukka/listdir');
 
-const BENCHMARKS = fs.readdirSync('src')
-  .filter(filename => filename.endsWith('.js'))
-  .map(filename => `./src/${filename}`);
-BENCHMARKS.sort();
+(async () => {
+  printPlatformInformation();
 
-try {
-  for (const benchmark of BENCHMARKS) {
-    const p = cp.spawnSync(process.execPath, [ benchmark ]);
-    console.log(p.stdout.toString().trim());
+  const BENCHMARKS = (await listDir('benchmark'))
+    .filter(filename => filename.endsWith('.js'))
+    .map(filename => `./benchmark/${filename}`);
+  BENCHMARKS.sort();
+  
+  try {
+    for (const benchmark of BENCHMARKS) {
+      const p = cp.spawnSync(process.execPath, [ benchmark ]);
+      console.log(p.stdout.toString().trim());
+    }
+  } catch (err) {
+    console.error(err);
   }
-} catch (err) {
-  console.error(err);
+})();
+
+function printPlatformInformation() {
+  console.log('Platform info:');
+
+  const os = require('os');
+  const { node, v8 } = process.versions;
+  const plat = `OS: ${os.type()} ${os.release()} ${os.arch()}\nNode.js: ${node}\nV8: ${v8}`;
+  const cpus = os.cpus().map(cpu => cpu.model).reduce((o, model) => {
+    o[model] = (o[model] || 0) + 1;
+    return o;
+  }, {});
+  const cpusInfo = Object.keys(cpus).map((key) => {
+    return `${key} x ${cpus[key]}`;
+  }).join('\n');
+
+  console.log(`${plat}\nCPU: ${cpusInfo}\nMemory: ${os.totalmem() / (1024 * 1024)} MiB\n`);
 }
