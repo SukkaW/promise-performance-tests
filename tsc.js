10093631800b18c9b91a9e39f136ab51b61dbb17
SukkaW <isukkaw@gmail.com>
2023-08-27 22:56:03 +0800

    Housekeeping

--- tsc.js
+++ tsc.js
@@ -1,11 +1,11 @@
 const ts = require('typescript');
-const fs = require('fs/promises');
+const fsp = require('fs/promises');
 const path = require('path');
 
 (async () => {
   const sourcePath = path.resolve(__dirname, process.argv.slice(2)[0]);
   const outputPath = path.resolve(__dirname, process.argv.slice(2)[1]);
-  const source = await fs.readFile(sourcePath, 'utf-8');
+  const source = await fsp.readFile(sourcePath, 'utf-8');
 
   const result = ts.transpileModule(source, {
     compilerOptions: {
@@ -13,5 +13,5 @@ const path = require('path');
     }
   });
 
-  await fs.writeFile(outputPath, result.outputText);
+  await fsp.writeFile(outputPath, result.outputText);
 })();
