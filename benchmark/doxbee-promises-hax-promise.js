47326561491f321d092575d3c2002a15fc3b1f0e
SukkaW <isukkaw@gmail.com>
2023-09-03 21:35:30 +0800

    Add `my-promise`

---
+++ benchmark/doxbee-promises-hax-promise.js
@@ -0,0 +1,10 @@
+Promise = require('my-promise').Promise;
+
+const doxbee = require('../lib/doxbee-promises.js');
+const measure = require('../lib/measure-promises.js');
+
+measure(doxbee, 'b', 'c')
+  .then(({ time, mem }) => {
+    console.log(JSON.stringify({ time, mem }));
+  })
+  .catch(reason => console.error(reason));
---
+++ benchmark/parallel-promises-hax-promise.js
@@ -0,0 +1,10 @@
+Promise = require('my-promise').Promise;
+
+const parallel = require('../lib/parallel-promises.js');
+const measure = require('../lib/measure-promises.js');
+
+measure(parallel, 'b', 'c')
+  .then(({ time, mem }) => {
+    console.log(JSON.stringify({ time, mem }));
+  })
+  .catch(reason => console.error(reason));
